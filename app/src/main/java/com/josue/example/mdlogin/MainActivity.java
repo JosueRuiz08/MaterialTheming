c9aee16f8735d9cf05d8b0dd319f5c29defece9e
JosueRuiz08 <josue.ruiz>
2022-10-19 14:16:57 -0500

    Implementación de Material Desing

--- app/src/main/java/com/josue/example/mdlogin/MainActivity.java
+++ app/src/main/java/com/josue/example/mdlogin/MainActivity.java
@@ -1,11 +1,10 @@
 package com.josue.example.mdlogin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
